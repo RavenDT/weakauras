f1710424b7099b752b2b12cbfd56dd37cf01a4f9
RavenDT <ravendt@gmail.com>
2020-04-01 23:54:49 -0400

    Updated "Misc/Soulstone Indicator" to fix #2

--- Misc/Soulstone Indicator.lua
+++ Misc/Soulstone Indicator.lua
@@ -5,18 +5,62 @@
 
 Compressed String
 
-!9Ev3YjUnu4NL6RsMHsaYpBVjxqio7MoGHb5KTt3zfwylBuJrIAjZcPT(zVhjzh7eij0CrhyW2sNZ35xRZh4UyFmIH96E6f962bJKyuV2DVO9fy0smks)feqjcxtZKmbh79lDoTdi4MXXXsQc717CTA004jcgxHr(JNm09gqPhfILypytsUAHiB8kfOVeGZZyu12vunYjjPW1Cj9AQmephJEGUfauKZJC5K5PuTtqJj5Pk8vyeNSeeFskzBraslurGGxe4UzflJOTag9dwKAH2kAVR2uGpsdbhCnjnNw6hOrePIMPLdHU53WENclLlzHyVZWO(lNZO8qk27Cm6AgjvKay2WbhSGW50uV8L1(OgkRtw6ELc9SySHlcagksfzwhs)TN53tn)EMvOkfofaFBvM)N7Eo4xeEiKDFzYpJMa5cFtGZc1zLqHins8doafSiKjZzqM9yHW)Zh94z5)(FwlIpDJ6AM0M8NxVUBucv)mjSQuIKksMcUbSnJZkVlgUvUWCp8KkJLKaDp1vE7c1vgsEgPN9ICfnnLfvvE615tD(Kge6AQo((cLKc5bKmFUzLjz0y2givpXD4qyzT23A0geox7qOvqRcymD23Dd45iRmv9sLYwbikp2a4Sb9r(Zq(9N6RlTZHLTjZV4oCYn3n0IFDKOQB2QJUWCPsSC2cweT6HQqU6PQRX5Cts9iJx0QiOD72hx8Rfb2pS4IaZofbxEzrGZGXJUQV)SHJ)8m37D98NDN3n3o03DQ71ofbQfuETUvFsfHK0IaisDlrcEW8oXr92XGVujrEwi1dYxnv783vntU(6M6091T1RcJTe(K1VdQR6hoYXUHZEWsNWQToOKPtAx5i63qBIFXFF5Zc3D0y)j3Nz36mSUwz6nl7NUBWaxeY59q5fXp0Lkjj2G3Xhoo5HIGTI8IGyrMbkJhNcDsC6p58(WIOMJUuJS4Euj(TC(6xUf82PoT4S0w1jHJFDiPPs6RgZ9VBA)ztDhn((3OT8qI5rBBeHfblisWY6t8Pr)phVnRZ6ZRMr5RBdNVgZsA3CM1HgTViQdFAGIjW3RfQh68Fg(M6BmWFD4qu9XXoY0P1hqvye7hspZi5pKMvJW)qkBh57C4A(phUOAgmgcc3WsHtYULd50ysi9K(rrJ5Yt(kL8qF9aXtgrJyKtmYkp5kXggpPFgLtmR0g4p50QzD9B19qF)aAKHxnEJ3U33MpBXNlXt3Lrv5zaTmvwoCeki07mH1SdW98nMR904u7a0scbheVG9mRgfXK)HzM76DMg7BLCOib4bwpzwD8EIVV197LXhXGvPUJeWiFlZnd7RR00JGkmNKEFfr6ExyOgl0Bs4SLw2Rn4uvsvGdN5b(BULE7SkYSaSarhWaljm(Hl9tCZom51bqKHkR9Wxn7x6Q(WO1q1uTkA(GXzWHMaXhIIyyTfrLeLgrlBX0vlG170(Cnts95yMGSjb)R2LpnqMYWPu))omWaL2PgsTLm2wqzjluwkXAVLPQ3lvqm)VfTBQVUeiMZQQ8HPePCNvLShPVCrvf76B0bOL)hAWuxxp9wRHy9SE6Am(F)
+!9E1wZjUnu4Fl1pLmdLeiHSTpKhieZM0HBdYjBNUZASITSrncjQLCwiT1)27rs2yULanpSdm2MJoNVZnzD(WVHVNpI6pOXfx1SX5(iPpQz9gxv)kF0mFuK(lOGse(cjvsfC)b)Y5xCoO4RcXm)bWdyE4urA3u8mI3Y5eFeQZyx3b(OqbtKcwpOH5BtZ1lmxVuFv5JwmmowsuWIT0iLPaKgoxb(rAnu7BdOkrscdUNjj3sKH(p5JEMSeIxrgpYLJFIrGGLdbHpAedVmpaPxjpqWZdCxmNMI1Wc5djgNXu(34J(onsn1gjq0TYvscJecb3lywgPioq9XsfjvRhc19315bQFMKgQZfu7zprj8qI)Gw(OBPyMirNDvbyNPyoNWgKnBJmWgUfbAHovrO2xRfIaG4WYsdsQWPk4bqAmLtLtTpdpALcpVSS4(ZnA1QSpnsq5WAEdh1ZTlGtkjbG024OH6cuOqWIeFNd(geEJgtiZ5y2JL7aAELwlEmnXejR3bUz3egsdvfQUrjeDQB8fOTkLMKaax1UTcQAh4SuCt7n5CcJrJk7jnp)tN)jn4Kxi6K6ocMbvlKm7jJKrPKy6cOapYTxpqS267nwRmTb3fqbfzLQJLmDXdnh29a(30Ck0TequwSbWjDAJ8MG8Ap2t3UEceBRG352Bu3h6zWNxLjQQDyvz3BeL2KomtQG3VwDpoJB69NymPwEq961pn)3YdSFOX5bMvYdU(68aNod7FtBVj9g(5jUp6oWBYdd6EFpp3XU36KhOMs4v2w(HjcXS8aiSClqc(H5vHtAUJd32irwAizauXw3SwhYSiIuTTr)6b9LPHD76204GbOTPUYxpaDA9poXXUGZESuxtR8fyKz72U6H1V8Ub(xVwMDy1Z)NR3OaUJf7VDTryw1Z0DFZoPI9Op0PJlc5Ciu2QCb78L4eBTYXdEx(58GLIS8GyrQbkteZG9MCYp5CyyreZrcQ(wCpPa)AoF5U7HODStnoLvRQiC6BdjHjjVzo3(HXTNm2T)WhFNn6hto3F5AzyEWuSe8SEkcj6hC(UEFwFg4ec)L62tFRV(rVhB2UvwhU6GAtIVxpuDy()B4x3EJd(7JhIYpo2zVo1(aMcZQ)q2zMT)HSSKlWhYyl3bNJ3Y)94vvZkYq1OlLbh8DVEQEmoKCw7OOHC5zFHGFUTEi7z9jru8zgDLNDJybLN0oLWXgj1bIyo1wVV(1Q9qF7i2idVA8oVDVVf3q4MAS6PuIklfO6PsZGJqbLoWuBZkaz33zsz503j2rYfKm2IRrHktPrvtT3Z0Fuev(NMz4VSZ0DpRM9ejaDYQj9Qt3t291gFRi7WgSkSTVaCVLsnMnFkgiMxVvfPlpYc1TuPLIMH5mzEBy2wOASMvSwemjnEBUH0id5u7HGg65cnnPynvFGjdwHn0WGzDyi8WkdXomNoZs1EnwQOOml)7jLuSHZxaMyfSD4A8bppdt5hV2R49EC6R7k6m6uHW7ZN86Lz)XFP52Q)FnMah2Um2qgUG03ucnzkWB7IMg2Uruv1Amb28)I01q99zaFDAzRoKHLYDKkPVs2wOAfb)lmKKFbQNx2u3g9)Vd
 
 ]]--
 
 -- Soulstone Indicator
 {
-    ["xOffset"] = 250,
-    ["yOffset"] = -155,
+    ["desaturate"] = false,
+    ["color"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 1,
+    },
     ["anchorPoint"] = "TOPLEFT",
-    ["cooldownSwipe"] = true,
-    ["cooldownEdge"] = false,
+    ["authorOptions"] = {
+        [1] = {
+            ["type"] = "toggle",
+            ["key"] = "soundEnabled",
+            ["useDesc"] = false,
+            ["default"] = true,
+            ["name"] = "Play Sound on Expiration",
+            ["width"] = 1,
+        },
+        [2] = {
+            ["type"] = "select",
+            ["key"] = "soundChannelNum",
+            ["values"] = {
+                [1] = "Master",
+                [2] = "SFX",
+                [3] = "Music",
+                [4] = "Ambience",
+                [5] = "Dialog",
+            },
+            ["default"] = 2,
+            ["name"] = "Sound Channel",
+            ["useDesc"] = false,
+            ["width"] = 1,
+        },
+    },
+    ["xOffset"] = 250,
+    ["zoom"] = 0,
+    ["yOffset"] = -155,
+    ["regionType"] = "icon",
     ["icon"] = true,
+    ["cooldownSwipe"] = true,
+    ["cooldownTextDisabled"] = false,
+    ["uid"] = ")ooTG(z4uZq",
+    ["cooldownEdge"] = false,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
     ["triggers"] = {
         [1] = {
             ["trigger"] = {
@@ -29,10 +73,10 @@ Compressed String
                 ["subeventPrefix"] = "SPELL",
                 ["spellIds"] = {
                 },
-                ["unit"] = "player",
+                ["useExactSpellId"] = true,
                 ["names"] = {
                 },
-                ["useExactSpellId"] = true,
+                ["unit"] = "player",
                 ["debuffType"] = "HELPFUL",
             },
             ["untrigger"] = {
@@ -40,14 +84,14 @@ Compressed String
         },
         [2] = {
             ["trigger"] = {
-                ["subeventPrefix"] = "SPELL",
+                ["custom_hide"] = "custom",
                 ["type"] = "custom",
-                ["custom"] = "function(event, ...)\\n    if event == \\\"COMBAT_LOG_EVENT_UNFILTERED\\\" then\\n        local subEvent = select(2, ...)\\n        local sourceName = select(5, ...)\\n        local spellID = select(12, ...)\\n        \\n        local playerName = UnitName(\\\"player\\\")\\n        if spellID == 20707\\n        and playerName ~= sourceName\\n        then\\n            if subEvent == \\\"SPELL_CAST_SUCCESS\\\" then\\n                local message = \\\"Thank you for the soulstone!\\\"\\n                SendChatMessage(message,\\\"WHISPER\\\",nil,sourceName)\\n            elseif subEvent == \\\"SPELL_AURA_REMOVED\\\" then\\n                local message = \\\"My soulstone has expired!\\\"\\n                SendChatMessage(message,\\\"WHISPER\\\",nil,sourceName)\\n                if aura_env.config.soundEnabled then\\n                    local channelNum = aura_env.config.soundChannelNum\\n                    local soundChannel = {\\n                        \\\"Master\\\",\\n                        \\\"SFX\\\",\\n                        \\\"Music\\\",\\n                        \\\"Ambience\\\",\\n                        \\\"Dialog\\\"\\n                    }\\n                    PlaySoundFile(\\\"Interface/AddOns/WeakAuras/Media/Sounds/BoxingArenaSound.ogg\\\",soundChannel[channelNum])\\n                end\\n            end\\n        end\\n    end\\n    \\n    return true\\nend",
+                ["custom"] = "function(event, ...)\\n    if event == \\\"COMBAT_LOG_EVENT_UNFILTERED\\\" then\\n        local subEvent = select(2, ...)\\n        local sourceName = select(5, ...)\\n        local destName = select(9, ...)\\n        local spellID = select(12, ...)\\n        local playerName = UnitName(\\\"player\\\")\\n        if spellID == 20707\\n        and playerName == destName\\n        and playerName ~= sourceName\\n        then\\n            if subEvent == \\\"SPELL_CAST_SUCCESS\\\" then\\n                local message = \\\"Thank you for the soulstone!\\\"\\n                SendChatMessage(message,\\\"WHISPER\\\",nil,sourceName)\\n            elseif subEvent == \\\"SPELL_AURA_REMOVED\\\" then\\n                local message = \\\"My soulstone has expired!\\\"\\n                SendChatMessage(message,\\\"WHISPER\\\",nil,sourceName)\\n                if aura_env.config.soundEnabled then\\n                    local channelNum = aura_env.config.soundChannelNum\\n                    local soundChannel = {\\n                        \\\"Master\\\",\\n                        \\\"SFX\\\",\\n                        \\\"Music\\\",\\n                        \\\"Ambience\\\",\\n                        \\\"Dialog\\\"\\n                    }\\n                    PlaySoundFile(\\\"Interface/AddOns/WeakAuras/Media/Sounds/BoxingArenaSound.ogg\\\",soundChannel[channelNum])\\n                end\\n            end\\n        end\\n    end\\n    \\n    return true\\nend",
                 ["subeventSuffix"] = "_CAST_START",
                 ["events"] = "COMBAT_LOG_EVENT_UNFILTERED",
                 ["custom_type"] = "event",
                 ["event"] = "Health",
-                ["custom_hide"] = "custom",
+                ["subeventPrefix"] = "SPELL",
             },
             ["untrigger"] = {
             },
@@ -56,26 +100,38 @@ Compressed String
         ["customTriggerLogic"] = "function(t)\\n    return t[1]\\nend",
         ["activeTriggerMode"] = 1,
     },
+    ["selfPoint"] = "TOPLEFT",
     ["internalVersion"] = 26,
     ["keepAspectRatio"] = false,
     ["animation"] = {
         ["start"] = {
-            ["duration_type"] = "seconds",
             ["type"] = "none",
+            ["duration_type"] = "seconds",
         },
         ["main"] = {
-            ["duration_type"] = "seconds",
             ["type"] = "none",
+            ["duration_type"] = "seconds",
         },
         ["finish"] = {
-            ["duration_type"] = "seconds",
             ["type"] = "none",
+            ["duration_type"] = "seconds",
         },
     },
-    ["desaturate"] = false,
+    ["id"] = "Soulstone",
+    ["auto"] = true,
+    ["frameStrata"] = 1,
+    ["width"] = 32,
+    ["alpha"] = 0.5,
+    ["config"] = {
+        ["soundEnabled"] = true,
+        ["soundChannelNum"] = 2,
+    },
+    ["inverse"] = false,
     ["subRegions"] = {
     },
     ["height"] = 32,
+    ["conditions"] = {
+    },
     ["load"] = {
         ["spec"] = {
             ["multi"] = {
@@ -90,63 +146,7 @@ Compressed String
             },
         },
     },
-    ["regionType"] = "icon",
-    ["alpha"] = 0.5,
-    ["width"] = 32,
-    ["cooldownTextDisabled"] = false,
-    ["auto"] = true,
-    ["authorOptions"] = {
-        [1] = {
-            ["type"] = "toggle",
-            ["key"] = "soundEnabled",
-            ["useDesc"] = false,
-            ["name"] = "Play Sound on Expiration",
-            ["default"] = true,
-            ["width"] = 1,
-        },
-        [2] = {
-            ["type"] = "select",
-            ["key"] = "soundChannelNum",
-            ["values"] = {
-                [1] = "Master",
-                [2] = "SFX",
-                [3] = "Music",
-                [4] = "Ambience",
-                [5] = "Dialog",
-            },
-            ["useDesc"] = false,
-            ["name"] = "Sound Channel",
-            ["default"] = 2,
-            ["width"] = 1,
-        },
-    },
-    ["id"] = "Soulstone",
-    ["color"] = {
-        [1] = 1,
-        [2] = 1,
-        [3] = 1,
-        [4] = 1,
-    },
-    ["frameStrata"] = 1,
     ["anchorFrameType"] = "SCREEN",
-    ["actions"] = {
-        ["start"] = {
-        },
-        ["finish"] = {
-        },
-        ["init"] = {
-        },
-    },
-    ["config"] = {
-        ["soundEnabled"] = true,
-        ["soundChannelNum"] = 2,
-    },
-    ["inverse"] = false,
-    ["uid"] = ")ooTG(z4uZq",
-    ["conditions"] = {
-    },
-    ["zoom"] = 0,
-    ["selfPoint"] = "TOPLEFT",
 }
 
 -- EOF
