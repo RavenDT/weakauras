60a2331649be58a5f26e8af16ac471a0f6b634d5
RavenDT <ravendt@gmail.com>
2020-04-05 20:21:09 -0400

    Added "Warlock/Immolate Bars" (TSU)

---
+++ Warlock/Immolate Bars.lua
@@ -0,0 +1,442 @@
+--[[ Immolate Bars ]]--
+
+
+--[[
+
+Compressed String
+
+!vRXIsPos2)fRARvRsPiabKQwNQafUYoqWLax9UJtGMKgOhdjujDu0zg(23ZP7op4LI4wBTtHkD60N3p6tFAT0S6BzgAzwiNw5CLTmNBz6G)aZonW)flZB6EVHL5y)ahAqDI9tob(lSmn89OwMl7ozsiLBzCHE(8caIw0Y23ZYqRO(LAvSmjE2Z8dUZN5XTmVUHr)g9Ir21(U(baDm0SAwQCHkfku5s9cLkQRx(sRjxOx0YOa8g9QAvZRRNVqjTYvRuw9MIWBQuSOw1svH3uvRyjD1BkHieiw4cIn1Yqh4bBoZ3luiOCsahgaVFcZJfotoggkNfgZdytNsdcLCw)KjWHo0XrtM0)1fGSFBJ231CqB49IhjrbKcivPUUTCcL4nmAm9zQh3eGIT0YC411m7p0SFTEaYIqIAUWL8kGCtpYCQckbiabOex(SuKCxavGeZ7A0UnUUiVuwJlzEBF3O5EMsrxZYmG4WIcbfjAEIcPTzZbIogyxx2uVulcOxeiYiVGE9OtvAmaNU(eH7aa(qBxsyicpiN24KZJC5SyvN8TBoBi7n6Mtctpw5lLXna)bm5xwTI2LvkxnFjWNOeW5XMC89LqEKJowS5eof5fWHBcBQsJBtCLIU0jZQE8OgotbZuDx2BVrcCwnQVVpWpGRCGqyLgvNxb7aZw4ihdOjY)a1nDfkpHhLG4auz9PmDaxaCYHspIqkWyGNGYbXdJyaoCoH5D4RoXl9WwpQb8bUr(meqYCKcwVBe6jGx94nHacqLB2R2nTgycstGDBQ3uWxZOy58XIDCOnKtiIbPeQ17(hU9TGFUZ8oar9TFgIrqnGXL5lIEx4AAnFUVlyvwnQobIGaM0DIk2VF37K8Z9mhKs6X5gAgaE(sM086EnAajBMGtbXDeojJPSLNGFkIXACaWUl4PoPb(CPbb9GFnM3fU9OEIT2sFjnab4nAGhX97XstHYcp5KOFinsWtkEwlV6zj(FqqSnzgvsdHaX9NofChXqNBOH2OZ6t0xbLrVbgxpSHrT6TBCdMwzcbcoqFvmna8(GipBHESpjykLVAKHi9G5lkgXcfUu64fnFmMejdDMtwAzuTAv0)KUqiRjaBoNjvusQ1HSKnpA(QrsVGvJM4hSoLtzrqLUMm0P2ddBd2mHRxIEp)Es8luE9YAQwBMo(oaPQnOFxWWGBJu)9thlfE7OqU)8piz7bLtDJuYRNlxSEWeCD3o1R1Fy7UFByJVdY1WbgnB1gep0sMjlU9mQ9tjewYKXXUG4sJw4G5Vsy)jGjhvcNsCDnXfeE(QrcOHVZLl3zR(NRgj)WMOEZQrxD1QrN8oS0jRgXNr9sHn(JRpKPC1iqr0qIj8dGmiKLAZpD1OcBr2nb1pkWM(TbTUzDal9ra6qd5sW2KMxEiGIoKBdA1pKDfwZBINnfwTcheOXKndGf3lGBpZfxSAuhYtuuLha)9f4xcoWMeYzEtxnkj55wWI27102apG1BmK69CoP362ar8CYi0iiA6vQuS82RC3oizz8Mmez0LmfRoMaji4(RgjDIxnctyGfrXG6kEJk5yp6l7hRjo5)sS3WVkuT7CEe7)(F((SOu3ssSzaJqcvPY2fs3lYIT5CHGD1Ne69)gHrmjAte3kY6mS2GE1g2RrNUF)Dcx3j2qUkx4m)xouOI)iH0EgXBk1riK8Gi6NfEjLbGNqCdpaOPGt0Exe9Gqbk2PPpqL4aWRdF60t4InSo5SpRYqzWXu3q8mUVd8LTFKiX7q434kVW8Xlx0Nnx6xudMEaN5MdJoWhQ)kMJ40KSfNhZsNFYT161boSWFCx7A)OrVto7W5orKKi1JiGwWxQXX8L6XeE7Zj(zdIAHBS4rg7sDaHfSVbCavJ4QsbK8bYrm(FhcUcGDtDFf1GqsHpnbr8KKftwdFU1km6yLKnmSqLqTrP4Q9rovnmhpHKbJbqQXCUuVtJ3Kc8e)PuY)1KL4pPBaEvcnHSkj0888Nlj4z4wtqogy)PtoEY(UHS)xfOppeF1Ky7jz2xbjEXwMeZ0rIimlKarIbhRiXHJyhkqJmTXrINfb(tdOHH4H0ey7eoKNX5KJfFP5TUktsSJezqwpMedjjMJteESEuqbzqbvIamrrxFfdjC6q)Bzo0VSRfXrEiwI7DkZHaL)(XhBdhcuGI8N)fWbz5x0k(NhoihuEL3ViJD9Y1MC9vKmkGYJc8ITGWI2DVa3rt5uhtFZdTg3UW37mR(V411Z9v8uWh2XxrKkQ(G5OoVSPSMFjJHNTv2eoSrbnwsS5MYJ4IKyn5rwUYUKhthw4VjoQ6Zzo8Q87(Yv22FkZoZbA5NTdf5VO9RkfjrGlfSIwaiBGlM6BO9MDhe)BsNafRdKL4E5n()VBnhCSPT62PrEH8KpvEG9oiCeJuzJwt630p2U9SzB2eA9bkme(n1VJUKpmCgXX)LhIBoJwmaaMW3RwLC4FZt9O9oAlBETcfluiFPYxQxqltN43ZBIfib(M4JoWndyVTA0)kI4Gn3B1O(RZJz1o53T2jZQ)rYTpOfpFA)gLDrkH0s)6Ud63ULrd18pZczqPUIUmHpVwlRAzy0OhuwyZ(X9Y6hz6ULy9)g40ZM8AA7TsOMSLXALIb9HmGk6ujTVqBB0fygrpnpkB1ITTvnVSSEXQv0ROxsxxpFfT0Bs5JAR(F1Tv9A9TB)FHXQ0XzSc3xG12z0(RPr46UgdR3TF)UD(IwIchOLagnJYMot1vAv7Tbx707Sk5gPcHdkHDI)(A9A396Fo92OsMPUeJ4TCTGYht4Cx5TlHOaevNHX35LcxgfkxjgINPZy2U0bmeImtK9QYIbRykXHXzi7guQ(EV6mXEc1HJA6O6y(n3iWqnhu3sDOoDilt2846p2HBx3cY63BICJgzpTXstGup)d1ZB3AEms4zsadBKaWeZWRr(zIBKANDEMvwi7kxVw(yymljBNoEoVuAjFhQwHJ24O259oxYRQzahsuOMqSPpwZXbKIhVNsEc7tt4J35)cnqmSd1HrE0eHi8XzuOCHXucpN)0PXygjSh1fkYQ5diJaNgAbnGNqhEMY8s0pFs5tOp)Pdwmn5W63lRGoiZ995ZIpOGYNmJ3buMZ41V4tvXdRDLDXXNFW9Ndt6VyOKdQN520nD8hQQpSEALIB236rR1dp5KR1FSKrsrtuvyidD7LBo)MpGwJ85YRN5MG34g2QV51vzuOqsG0TQCifvjOIcYCpX74EnvG1pEPP(zxlBLpSMhh06I0NUWebqbNi752xFQKvyoouOcmJgFhv8BEpOe3fZuJ23vNIN3uyVr2w7Sw3D)nop4vRjAvqskYiTmtEv)agCqczfZM32TxR)DxJ(1AlQZhTgI)DomJ)35aV0E8YX9CE)DEYNtp2x8zCV1cyCV1)5d
+
+]]--
+
+-- Immolate Bars (Dynamic Group)
+{
+    ["grow"] = "DOWN",
+    ["controlledChildren"] = {
+        [1] = "Immolate Bar",
+    },
+    ["borderBackdrop"] = "None",
+    ["xOffset"] = -500,
+    ["yOffset"] = 200,
+    ["gridType"] = "RD",
+    ["borderColor"] = {
+        [1] = 0.51372549019608,
+        [2] = 0.50980392156863,
+        [3] = 0.52549019607843,
+        [4] = 1,
+    },
+    ["rowSpace"] = 1,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+        },
+        ["finish"] = {
+        },
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["names"] = {
+                },
+                ["type"] = "aura2",
+                ["spellIds"] = {
+                },
+                ["subeventSuffix"] = "_CAST_START",
+                ["unit"] = "player",
+                ["subeventPrefix"] = "SPELL",
+                ["event"] = "Health",
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+    },
+    ["columnSpace"] = 1,
+    ["internalVersion"] = 26,
+    ["useLimit"] = false,
+    ["align"] = "CENTER",
+    ["stagger"] = 0,
+    ["subRegions"] = {
+    },
+    ["load"] = {
+        ["use_class"] = false,
+        ["spec"] = {
+            ["multi"] = {
+            },
+        },
+        ["class"] = {
+            ["multi"] = {
+            },
+        },
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["backdropColor"] = {
+        [1] = 1,
+        [2] = 0.99607843137255,
+        [3] = 1,
+        [4] = 0,
+    },
+    ["animate"] = false,
+    ["anchorPoint"] = "CENTER",
+    ["scale"] = 1,
+    ["radius"] = 200,
+    ["border"] = true,
+    ["borderEdge"] = "Blizzard Tooltip",
+    ["regionType"] = "dynamicgroup",
+    ["borderSize"] = 3,
+    ["limit"] = 5,
+    ["space"] = 5,
+    ["groupIcon"] = 135817,
+    ["constantFactor"] = "RADIUS",
+    ["rotation"] = 0,
+    ["borderOffset"] = 6,
+    ["authorOptions"] = {
+    },
+    ["borderInset"] = 3,
+    ["id"] = "Immolate Bars",
+    ["gridWidth"] = 5,
+    ["frameStrata"] = 1,
+    ["anchorFrameType"] = "SCREEN",
+    ["selfPoint"] = "TOP",
+    ["config"] = {
+    },
+    ["uid"] = "ARWXHzrKMmM",
+    ["arcLength"] = 360,
+    ["conditions"] = {
+    },
+    ["sort"] = "none",
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+}
+
+-- Immolate Bar (Bar)
+{
+    ["sparkWidth"] = 10,
+    ["sparkOffsetX"] = 0,
+    ["xOffset"] = 0,
+    ["yOffset"] = 0,
+    ["anchorPoint"] = "CENTER",
+    ["sparkRotation"] = 0,
+    ["sparkRotationMode"] = "AUTO",
+    ["backgroundColor"] = {
+        [1] = 0,
+        [2] = 0,
+        [3] = 0,
+        [4] = 0.5,
+    },
+    ["triggers"] = {
+        [1] = {
+            ["trigger"] = {
+                ["type"] = "custom",
+                ["subeventSuffix"] = "_CAST_START",
+                ["event"] = "Health",
+                ["subeventPrefix"] = "SPELL",
+                ["unit"] = "player",
+                ["custom"] = "function(allStates, event, ...)\\n    if event == \\\"COMBAT_LOG_EVENT_UNFILTERED\\\" then\\n        local subEvent   = select( 2, ...)\\n        local sourceGUID = select( 4, ...)\\n        local destGUID   = select( 8, ...)\\n        local destName   = select( 9, ...)\\n        local spellID    = select(12, ...)\\n        local spellName  = select(13, ...)\\n        \\n        -- Make sure we are casting Immolate\\n        if sourceGUID == aura_env.player\\n        and spellID == 157736\\n        then\\n            -- Find existing bar to update or initialize if new\\n            allStates[destGUID] = allStates[destGUID] or {}\\n            -- Make a local alias for allStates[destGUID]\\n            local state = allStates[destGUID]\\n            \\n            if subEvent == \\\"SPELL_AURA_REMOVED\\\" then\\n                if state.show then\\n                    state.changed = true\\n                    state.show = false\\n                end\\n            else\\n                if destGUID == UnitGUID(\\\"target\\\") then\\n                    local name, icon, count, _, duration, expTime = AuraUtil.FindAuraByName(spellName,\\\"target\\\",\\\"HARMFUL|PLAYER\\\")\\n                    if name and count and duration and expTime then\\n                        -- If enabled, shorten target name if it's really long\\n                        if aura_env.config.TRUNC_ENABLED then\\n                            local maxLen = aura_env.config.TRUNC_MAX_LEN\\n                            if string.len(destName) > maxLen then\\n                                destName = string.sub(destName,0,maxLen) .. \\\"...\\\"\\n                            end\\n                        end\\n                        \\n                        state.changed = true\\n                        state.show = true\\n                        state.name = destName\\n                        state.icon = icon\\n                        state.stacks = count\\n                        state.progressType = \\\"timed\\\"\\n                        state.duration = duration\\n                        state.expirationTime = expTime\\n                        state.caster = sourceName\\n                        state.autoHide = true\\n                        state.additionalProgress = {\\n                            min = 0,\\n                            max = duration\\n                        }\\n                    end\\n                end\\n            end\\n        end\\n    end\\n    \\n    return true\\nend",
+                ["events"] = "COMBAT_LOG_EVENT_UNFILTERED",
+                ["custom_type"] = "stateupdate",
+                ["check"] = "event",
+                ["names"] = {
+                },
+                ["spellIds"] = {
+                },
+                ["debuffType"] = "HELPFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+        [2] = {
+            ["trigger"] = {
+                ["type"] = "aura2",
+                ["auraspellids"] = {
+                    [1] = "157736",
+                },
+                ["ownOnly"] = true,
+                ["event"] = "Health",
+                ["subeventPrefix"] = "SPELL",
+                ["subeventSuffix"] = "_CAST_START",
+                ["useExactSpellId"] = true,
+                ["unit"] = "multi",
+                ["debuffType"] = "HARMFUL",
+            },
+            ["untrigger"] = {
+            },
+        },
+        ["disjunctive"] = "custom",
+        ["customTriggerLogic"] = "function(t)\\n    return t[1]\\nend",
+        ["activeTriggerMode"] = 1,
+    },
+    ["icon_color"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 1,
+    },
+    ["useTooltip"] = false,
+    ["animation"] = {
+        ["start"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["main"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+        ["finish"] = {
+            ["type"] = "none",
+            ["duration_type"] = "seconds",
+        },
+    },
+    ["barColor"] = {
+        [1] = 1,
+        [2] = 0,
+        [3] = 0,
+        [4] = 1,
+    },
+    ["desaturate"] = false,
+    ["sparkOffsetY"] = 0,
+    ["subRegions"] = {
+        [1] = {
+            ["type"] = "aurabar_bar",
+        },
+        [2] = {
+            ["text_shadowXOffset"] = 1,
+            ["type"] = "subtext",
+            ["text_text"] = "%n",
+            ["text_color"] = {
+                [1] = 1,
+                [2] = 0.98823529411765,
+                [3] = 0.98823529411765,
+                [4] = 1,
+            },
+            ["text_font"] = "Friz Quadrata TT",
+            ["text_shadowColor"] = {
+                [1] = 0,
+                [2] = 0,
+                [3] = 0,
+                [4] = 1,
+            },
+            ["text_shadowYOffset"] = -1,
+            ["text_selfPoint"] = "AUTO",
+            ["text_fontSize"] = 14,
+            ["text_visible"] = true,
+            ["text_anchorPoint"] = "INNER_LEFT",
+            ["anchorYOffset"] = 0,
+            ["text_justify"] = "CENTER",
+            ["rotateText"] = "NONE",
+            ["anchorXOffset"] = 0,
+            ["text_fontType"] = "OUTLINE",
+        },
+        [3] = {
+            ["text_shadowXOffset"] = 1,
+            ["type"] = "subtext",
+            ["text_text"] = "%p",
+            ["text_color"] = {
+                [1] = 0.96078431372549,
+                [2] = 0.99607843137255,
+                [3] = 1,
+                [4] = 1,
+            },
+            ["text_font"] = "Friz Quadrata TT",
+            ["text_shadowColor"] = {
+                [1] = 0,
+                [2] = 0,
+                [3] = 0,
+                [4] = 1,
+            },
+            ["text_shadowYOffset"] = -1,
+            ["text_selfPoint"] = "AUTO",
+            ["text_fontSize"] = 14,
+            ["text_visible"] = true,
+            ["text_anchorPoint"] = "INNER_RIGHT",
+            ["anchorYOffset"] = 0,
+            ["text_justify"] = "CENTER",
+            ["rotateText"] = "NONE",
+            ["anchorXOffset"] = 0,
+            ["text_fontType"] = "OUTLINE",
+        },
+        [4] = {
+            ["text_shadowXOffset"] = 1,
+            ["type"] = "subtext",
+            ["text_text"] = "%s",
+            ["text_color"] = {
+                [1] = 1,
+                [2] = 1,
+                [3] = 1,
+                [4] = 1,
+            },
+            ["text_font"] = "Friz Quadrata TT",
+            ["text_shadowColor"] = {
+                [1] = 0,
+                [2] = 0,
+                [3] = 0,
+                [4] = 1,
+            },
+            ["text_shadowYOffset"] = -1,
+            ["text_selfPoint"] = "AUTO",
+            ["text_fontSize"] = 12,
+            ["text_visible"] = true,
+            ["text_anchorPoint"] = "ICON_BOTTOMRIGHT",
+            ["anchorYOffset"] = 0,
+            ["text_justify"] = "CENTER",
+            ["rotateText"] = "NONE",
+            ["anchorXOffset"] = 0,
+            ["text_fontType"] = "OUTLINE",
+        },
+    },
+    ["height"] = 20,
+    ["load"] = {
+        ["spec"] = {
+            ["single"] = 3,
+            ["multi"] = {
+                [3] = true,
+            },
+        },
+        ["use_petbattle"] = false,
+        ["class_and_spec"] = {
+            ["single"] = 267,
+        },
+        ["use_vehicleUi"] = false,
+        ["use_vehicle"] = false,
+        ["class"] = {
+            ["single"] = "WARLOCK",
+            ["multi"] = {
+                ["WARLOCK"] = true,
+            },
+        },
+        ["use_class_and_spec"] = true,
+        ["size"] = {
+            ["multi"] = {
+            },
+        },
+    },
+    ["sparkBlendMode"] = "ADD",
+    ["useAdjustededMax"] = false,
+    ["uid"] = "1)IPWDdXnAF",
+    ["authorOptions"] = {
+        [1] = {
+            ["type"] = "toggle",
+            ["key"] = "TRUNC_ENABLED",
+            ["useDesc"] = false,
+            ["name"] = "Truncate Target Names",
+            ["default"] = true,
+            ["width"] = 1,
+        },
+        [2] = {
+            ["type"] = "number",
+            ["useDesc"] = false,
+            ["max"] = 999,
+            ["step"] = 1,
+            ["width"] = 1,
+            ["min"] = 0,
+            ["name"] = "Maximum Length for Target Name",
+            ["key"] = "TRUNC_MAX_LEN",
+            ["default"] = 20,
+        },
+    },
+    ["icon"] = true,
+    ["displayIcon"] = "135817",
+    ["smoothProgress"] = false,
+    ["useAdjustededMin"] = false,
+    ["regionType"] = "aurabar",
+    ["parent"] = "Immolate Bars",
+    ["selfPoint"] = "CENTER",
+    ["icon_side"] = "LEFT",
+    ["sparkTexture"] = "Interface\\\\CastingBar\\\\UI-CastingBar-Spark",
+    ["sparkHeight"] = 30,
+    ["texture"] = "Blizzard",
+    ["spark"] = true,
+    ["zoom"] = 0.05,
+    ["auto"] = false,
+    ["anchorFrameType"] = "SCREEN",
+    ["sparkHidden"] = "NEVER",
+    ["alpha"] = 1,
+    ["frameStrata"] = 1,
+    ["width"] = 240,
+    ["id"] = "Immolate Bar",
+    ["config"] = {
+        ["TRUNC_ENABLED"] = true,
+        ["TRUNC_MAX_LEN"] = 22,
+    },
+    ["inverse"] = false,
+    ["actions"] = {
+        ["start"] = {
+        },
+        ["init"] = {
+            ["do_custom"] = true,
+            ["custom"] = "aura_env.player = UnitGUID(\\\"player\\\")\\n    \\n    ",
+        },
+        ["finish"] = {
+            ["stop_sound"] = true,
+        },
+    },
+    ["orientation"] = "HORIZONTAL",
+    ["conditions"] = {
+        [1] = {
+            ["check"] = {
+                ["op"] = "<",
+                ["checks"] = {
+                    [1] = {
+                        ["trigger"] = 1,
+                        ["variable"] = "show",
+                        ["value"] = 1,
+                    },
+                },
+                ["trigger"] = 2,
+                ["variable"] = "expirationTime",
+                ["value"] = "4",
+            },
+            ["changes"] = {
+                [1] = {
+                    ["value"] = {
+                        ["sound_type"] = "Play",
+                        ["sound"] = "Interface\\\\Addons\\\\WeakAuras\\\\PowerAurasMedia\\\\Sounds\\\\heartbeat.ogg",
+                        ["sound_channel"] = "SFX",
+                    },
+                    ["property"] = "sound",
+                },
+            },
+        },
+        [2] = {
+            ["check"] = {
+                ["trigger"] = 2,
+                ["variable"] = "expirationTime",
+                ["op"] = ">",
+                ["value"] = "4",
+            },
+            ["changes"] = {
+                [1] = {
+                    ["value"] = {
+                        ["sound_type"] = "Stop",
+                    },
+                    ["property"] = "sound",
+                },
+            },
+        },
+    },
+    ["internalVersion"] = 26,
+    ["sparkColor"] = {
+        [1] = 1,
+        [2] = 1,
+        [3] = 1,
+        [4] = 1,
+    },
+}
+
+-- EOF
